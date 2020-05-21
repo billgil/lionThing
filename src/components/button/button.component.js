1120dc3b6318bc6a4ec62cd47aebb63f97940ba2
Bill Gilmore <bill@no79design.com>
2020-05-21 11:14:05 +0200

    working form but need to add accessibilty props

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -1,40 +1,39 @@
 import { LionButton } from '@lion/button';
-import { css } from "lit-element";
+import { css } from 'lit-element';
 
 export class TimerButton extends LionButton {
-    static get styles() {
-        return [
-            ...super.styles,
-            css`
-                :host {
-                    display: block;
-                    max-width: fit-content;
-                    width: 100%;
-                }
-                .btn {
-                    align-items: center;
-                    background-color: transparent;
-                    color: #ffffff;
-                    display: flex;
-                    font-size: 12px;
-                    justify-content: center;
-                    max-width: fit-content;
-                    padding: 12px 24px;
-                    text-transform: uppercase;
-                    transition: all 0.2s ease-in;
-                }
-                :host .btn:hover,
-                :host .btn:active,
-                :host(:hover) .btn, 
-                :host([hover]) .btn,
-                :host(:active) .btn, 
-                :host([active]) .btn  {
-                    background-color: #18bd09;
-                    background: #18bd09;
-                }
-            `
-        ]
-    }
-
+  static get styles() {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          display: block;
+          max-width: fit-content;
+          width: 100%;
+        }
+        .btn {
+          align-items: center;
+          background-color: transparent;
+          color: #ffffff;
+          display: flex;
+          font-size: 12px;
+          justify-content: center;
+          max-width: fit-content;
+          padding: 12px;
+          text-transform: uppercase;
+          transition: all 0.2s ease-in;
+        }
+        :host .btn:hover,
+        :host .btn:active,
+        :host(:hover) .btn,
+        :host([hover]) .btn,
+        :host(:active) .btn,
+        :host([active]) .btn {
+          background-color: #18bd09;
+          background: #18bd09;
+        }
+      `,
+    ];
+  }
 }
 customElements.define('timer-button', TimerButton);
--- src/components/timer-maker/timer-maker.component.js
+++ src/components/timer-maker/timer-maker.component.js
@@ -1,27 +1,27 @@
-import { LitElement, html, css } from "lit-element"
-import '../timer/timer.component.js'
-import '../button-create/button-create.component.js'
-import '../input/input.component.js'
-import '@lion/form/lion-form.js'
+import { LitElement, html, css } from 'lit-element';
+import '../timer/timer.component.js';
+import '../button-create/button-create.component.js';
+import '../input/input.component.js';
+import '@lion/form/lion-form.js';
 
 class TimerMaker extends LitElement {
   static get properties() {
-		return {
-			timerTitle: { type: String },
+    return {
+      timerTitle: { type: String },
       secondsToCount: { type: Number },
       activeTimers: { type: Array },
-      timerIdGen : { type: Number }
-		}
-	}
-
-	constructor() {
-    super()
-    this.timerTitle = ''
-    this.secondsToCount = 0
-    this.timerIdGen = 0
-    this.activeTimers = []
+      timerIdGen: { type: Number },
+    };
   }
-  
+
+  constructor() {
+    super();
+    this.timerTitle = '';
+    this.secondsToCount = 0;
+    this.timerIdGen = 0;
+    this.activeTimers = [];
+  }
+
   static get styles() {
     return css`
       :host {
@@ -29,9 +29,7 @@ class TimerMaker extends LitElement {
       }
 
       .timer-maker {
-        align-items: center;
-        display: flex;
-        justify-content: space-around;
+        display: block;
         margin: 0 auto;
         max-width: 900px;
         padding: 30px 20px;
@@ -52,53 +50,51 @@ class TimerMaker extends LitElement {
         padding: 10px 50px 20px;
         width: 100%;
       }
-    `
+    `;
   }
 
   render() {
     return html`
-      <div style="padding:20px;background:#eee;text-align:center">
+      <div class="timer-maker">
         <lion-form>
           <form>
-            <lion-fieldset name="fullName">
-              <lion-input label="First Name" name="firstName"></lion-input>
+            <lion-fieldset style="display:flex;" name="timer-details">
+              <timer-input
+                id="first-input-focus"
+                class="timer-maker__input"
+                @keyup=${this.titleUpdated}
+                placeholder="Timer Name"
+                name="timer-title"
+                type="text"
+                required
+              ></timer-input>
+
+              <timer-input
+                class="timer-maker__input"
+                @keyup=${this.secondsUpdated}
+                placeholder="Amount of seconds"
+                name="timer-count-seconds"
+                type="number"
+                required
+              ></timer-input>
+
+              <timer-button-create type="submit" @click=${this.createNewTimer}
+                >Start Timer</timer-button-create
+              >
             </lion-fieldset>
           </form>
         </lion-form>
       </div>
-   
-
-      <div class="timer-maker">
-
-        <timer-input 
-          id="first-input-focus"
-          class="timer-maker__input" 
-          @keyup=${this.titleUpdated} 
-          placeholder="Timer Name" 
-          name="timer-title" 
-          type="text"
-        ></timer-input>
-
-        <timer-input 
-          class="timer-maker__input" 
-          @keyup=${this.secondsUpdated} 
-          placeholder="Amount of seconds" 
-          name="timer-count-seconds" 
-          type="number"
-        ></timer-input>
-
-        <timer-button-create @click=${this.createNewTimer}>Start Timer</timer-button-create>
-      </div>
 
       <div class="timers">
         ${this.activeTimers.map(
-          (timer) => html`
-            <timer-item 
-              @timer-deleted=${this.removeTimer} 
-              @timer-tick=${this.timerTick} 
+          timer => html`
+            <timer-item
+              @timer-deleted=${this.removeTimer}
+              @timer-tick=${this.timerTick}
               @timer-count-toggle=${this.toggleTimerCountDown}
-              .timerID="${timer.id}" 
-              .timerTitle="${timer.title}" 
+              .timerID="${timer.id}"
+              .timerTitle="${timer.title}"
               .secondsToCount="${timer.seconds}"
               .isCountingDown="${timer.isCountingDown}"
               .isCountComplete="${timer.isCountComplete}"
@@ -106,77 +102,80 @@ class TimerMaker extends LitElement {
           `
         )}
       </div>
-    `
+    `;
   }
 
   get timerInputs() {
-    return this.shadowRoot.querySelectorAll('.timer-maker__input')
+    return this.shadowRoot.querySelectorAll('.timer-maker__input');
   }
 
   get firstInputToFocus() {
-    return this.shadowRoot.querySelector('#first-input-focus')
+    return this.shadowRoot.querySelector('#first-input-focus');
   }
 
   /* eslint no-param-reassign: ["error", { "props": false }] */
   resetInputs() {
     this.timerInputs.forEach(timerInput => {
-      timerInput.value = ''
-    })
+      timerInput.value = '';
+    });
   }
 
   titleUpdated(event) {
-    this.timerTitle = event.target.value
+    this.timerTitle = event.target.value;
   }
 
   secondsUpdated(event) {
-    this.secondsToCount = event.target.value
+    this.secondsToCount = event.target.value;
   }
 
   // updating the data model for each timer with the timers new value
   timerTick(event) {
     if (this.activeTimers[event.detail.id]) {
-      this.activeTimers[event.detail.id].seconds = event.detail.count
+      this.activeTimers[event.detail.id].seconds = event.detail.count;
     }
   }
 
   // toggle the pause and unpause of each timer
   toggleTimerCountDown(event) {
-    this.activeTimers[event.detail.id].isCountingDown = event.detail.isCountingDown
+    this.activeTimers[event.detail.id].isCountingDown =
+      event.detail.isCountingDown;
   }
 
   // remove timer is a custom event passed to each timer
   removeTimer(event) {
     // filter all active timers unless the ID matches the ID passed by the event
-    this.activeTimers = this.activeTimers.filter((timer) => timer.id !== event.detail)
+    this.activeTimers = this.activeTimers.filter(
+      timer => timer.id !== event.detail
+    );
   }
 
   createNewTimer() {
     // early returns if the input have no values
     if (this.timerTitle.length === 0) {
-      return
+      return;
     }
     if (this.secondsToCount <= 0) {
-      return
+      return;
     }
-    
+
     // make a new timer for the active timers array
     const newTimer = {
       title: this.timerTitle,
       seconds: this.secondsToCount,
       id: this.timerIdGen,
       isCountingDown: true,
-      isCountComplete: false
-    }
+      isCountComplete: false,
+    };
     // this insures the timers always have a unique ID
-    this.timerIdGen += 1
-    this.activeTimers = [...this.activeTimers, newTimer]
+    this.timerIdGen += 1;
+    this.activeTimers = [...this.activeTimers, newTimer];
 
     // clean title and counter
-    this.timerTitle = ''
-    this.secondsToCount = 0
-    this.resetInputs()
-    this.firstInputToFocus.focus()
+    this.timerTitle = '';
+    this.secondsToCount = 0;
+    this.resetInputs();
+    this.firstInputToFocus.focus();
   }
 }
 
-customElements.define("timer-maker", TimerMaker)
+customElements.define('timer-maker', TimerMaker);
